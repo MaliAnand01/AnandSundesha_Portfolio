9948bfeec2b99bff4d369c8c429fc64e95f19607
MaliAnand01 <anandsundesha@gmail.com>
2026-01-28 18:07:25 +0530

    feat: add `Developer3D` component and its corresponding Spline scene model

--- src/components/hero/Developer3D.jsx
+++ src/components/hero/Developer3D.jsx
@@ -6,7 +6,7 @@ const Developer3D = () => {
   return (
     <div className="relative w-full h-[500px] md:h-full flex items-center justify-center">
       <SplineScene 
-        scene="https://prod.spline.design/kZDDjO5HuC9GJUM2/scene.splinecode"
+        scene="/models/scene.splinecode"
         className="w-full h-full"
       />
     </div>
